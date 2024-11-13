407c2a110ec4aa4e7047e3384342d850f0b7a0f5
hiagogabrielga <hiagogabriel1132@gmail.com>
2024-11-13 08:31:06 -0400

    atualizar

--- React/projeto/src/app/sobre/page.js
+++ React/projeto/src/app/sobre/page.js
@@ -1,8 +1,34 @@
+'use client'
+import { useState } from "react";
 
 export default function sobre() {
+    const [conteudo, setConteudo] = useState("bom dia")
+    const [name, apresentaNome] = useState("")
+    const [showDiv, setShowDiv] = useState(true)
+    const controlarParagrafo = () => {
+        setConteudo("boa tarde")
+        console.log(conteudo)
+    }
+
+    const controlarInput = (evento) => {
+        apresentaNome(evento.target.value)
+    }
     return (
         <main>
-            <h1>Pagina Sobre</h1>
+            {/*<h1>{conteudo}</h1>
+                <button onClick={controlarParagrafo}>Mudar</button>*/}
+
+            <button onClick={() => { setShowDiv(!showDiv) }} > {showDiv ? 'Esconder' : 'Mostrar'}</button>
+
+            {showDiv &&
+                (
+                    <div>
+                        <p>Nome: {name}</p>
+                        <input type="text" onChange={controlarInput} />
+                    </div>
+                )
+            }
+
         </main>
     )
 }
