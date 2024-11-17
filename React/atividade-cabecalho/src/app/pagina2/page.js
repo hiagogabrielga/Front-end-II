157eb657fc548c9ffcc6257eade4f87a48a9b315
hiagogabrielga <hiagogabriel1132@gmail.com>
2024-11-17 18:31:01 -0400

    sei la

--- React/atividade-cabecalho/src/app/pagina2/page.js
+++ React/atividade-cabecalho/src/app/pagina2/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PaginaTwo() {
     return (
       <div>
-        <h1>Bom dia 2 o inimigo agora é outro.</h1>
+        <h1>Bom dia o inimigo agora é outro.</h1>
       </div>
     );
   }
