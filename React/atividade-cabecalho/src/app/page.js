bf12af93081caabe58a0730bd78b45dfd89e6ba4
hiagogabrielga <hiagogabriel1132@gmail.com>
2024-11-19 09:06:31 -0400

    atualizar

--- React/atividade-cabecalho/src/app/page.js
+++ React/atividade-cabecalho/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="divsoes">
       <h2>Pagina 1</h2>
-      <p>Lembrar de adiciona4r alguama imagemoiu conteudo para popular está pagina.</p>
+      <p>Lembrar de adicionar alguama imagemoiu conteudo para popular está pagina.</p>
     </div>
 
   );
