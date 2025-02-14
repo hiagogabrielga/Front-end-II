ca514eb832645f74bbe7a64c8c2c2a7357075df6
hiagogabrielga <hiagogabriel1132@gmail.com>
2025-02-14 08:07:30 -0400

    atualização final

--- React/atividade-clinica/src/components/Footer/index.js
+++ React/atividade-clinica/src/components/Footer/index.js
@@ -4,9 +4,11 @@ import styles from "./footer.module.css"
 
 export default function Footer() {
     return (
-        <div className={styles.campoCopyRight}>
+        <div>
+            <div className={styles.campoCopyRight}>
             <h3>Vitalis Clínica Integrada - Cuidando de você em cada batida do seu coração.</h3>
             <p>&copy; Todos direitos reservados 2025.</p>
+            </div>
         </div>
     )
 }
